8df4cfa2d947443a74f28bd06b271d9c35d14eee
Bruno Moraes <moraes.7bruno@gmail.com>
2021-04-19 20:36:33 -0300

    iniciando projeto & criando splash e home screen.

---
+++ lib/home/components/score_card.dart
@@ -0,0 +1,57 @@
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/components/chart.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCard extends StatelessWidget {
+  const ScoreCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 20,
+        right: 20,
+      ),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Chart(),
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: 24,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Vamos Começar',
+                      style: AppTextStyles.heading,
+                    ),
+                    Text(
+                      'Complete os desafios e avance em conhecimento.',
+                      style: AppTextStyles.body,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Column(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:DevQuiz/home/components/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(),
+    );
+  }
+}
